9e959c21931394c3a65a031309b8945e2b0260fe
Yana <vsebydetxoposho@gmail.ru>
2020-06-08 16:12:26 +0300

    zadanye

--- js/setup.js
+++ js/setup.js
@@ -3,33 +3,39 @@
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
 
-document.querySelector('.setup-similar').classList.remove('hidden');
-
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template')
     .content
     .querySelector('.setup-similar-item');
 
 var WIZARD_NUMBERS = 4;
-var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var WIZARD_SURNAMES = [' да Марья', ' Верон', ' Мирабелла', ' Вальц', ' Онопко', ' Топольницкая', ' Нионго', ' Ирвинг'];
+var wizardsNames = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var wizardSurnames = [' да Марья', ' Верон', ' Мирабелла', ' Вальц', ' Онопко', ' Топольницкая', ' Нионго', ' Ирвинг'];
 var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
 
 
-// function getRandomInt(max) {
-//   return Math.floor(Math.random() * Math.floor(max));
-// }
-
-var wizards = [
-  {
-  name: WIZARD_NAMES[i] + ' ' + WIZARD_SURNAMES[i],
-  coatColor: coatColors[i],
-  eyesColor: eyesColors[i],
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
 }
-];
 
-for (var i = 0; i < WIZARD_NUMBERS; i++) {
+function wizardsPlayersCreate() {
+  var wizards = [];
+  var wizard = {};
+  for (var i = 0; i < WIZARD_NUMBERS; i++) {
+    wizard = {
+      name: wizardsNames[getRandomInt(wizardsNames.length)] + ' ' + wizardSurnames[getRandomInt(wizardSurnames.length)],
+      coatColor: coatColors[getRandomInt(coatColors.length)],
+      eyesColor: eyesColors[getRandomInt(eyesColors.length)]
+    };
+    wizards.push(wizard); //добавили объект в массив
+  }
+  return wizards;
+}
+
+var wizards = wizardsPlayersCreate();
+
+for (var i = 0; i < wizards.length; i++) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
 
   wizardElement.querySelector('.setup-similar-label').textContent = wizards[i].name;
@@ -39,4 +45,4 @@ for (var i = 0; i < WIZARD_NUMBERS; i++) {
   similarListElement.appendChild(wizardElement);
 }
 
-
+document.querySelector('.setup-similar').classList.remove('hidden');
