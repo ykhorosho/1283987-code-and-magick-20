cab3504776812717be280db68796e67f1dc117bc
Yana <vsebydetxoposho@gmail.ru>
2020-06-16 19:34:13 +0300

    zadanye

--- js/setup.js
+++ js/setup.js
@@ -46,3 +46,114 @@ for (var i = 0; i < wizards.length; i++) {
 }
 
 document.querySelector('.setup-similar').classList.remove('hidden');
+
+// Открытие закрытие окна настройки персонажа:
+
+var setup = document.querySelector('.setup');
+var setupOpen = document.querySelector('.setup-open');
+var setupClose = setup.querySelector('.setup-close');
+
+var onPopupEscPress = function (evt) {
+  if (evt.key === 'Escape') {
+    evt.preventDefault();
+    closePopup();
+  }
+};
+
+var openPopup = function () {
+  setup.classList.remove('hidden');
+
+  document.addEventListener('keydown', onPopupEscPress);
+};
+
+var closePopup = function () {
+  setup.classList.add('hidden');
+
+  document.removeEventListener('keydown', onPopupEscPress);
+};
+
+setupOpen.addEventListener('click', function () {
+  openPopup();
+});
+
+setupOpen.addEventListener('keydown', function (evt) {
+  if (evt.key === 'Enter') {
+    openPopup();
+  }
+});
+
+setupClose.addEventListener('click', function () {
+  closePopup();
+});
+
+setupClose.addEventListener('keydown', function (evt) {
+  if (evt.key === 'Enter') {
+    closePopup();
+  }
+});
+
+// Валидация ввода имени персонажа.
+
+var MIN_NAME_LENGTH = 2;
+var MAX_NAME_LENGTH = 25;
+
+var userNameInput = document.querySelector('.setup-user-name');
+
+userNameInput.addEventListener('invalid', function () {
+  if (userNameInput.validity.valueMissing) {
+    userNameInput.setCustomValidity('Обязательное поле');
+  } else {
+    userNameInput.setCustomValidity('');
+  }
+});
+
+userNameInput.addEventListener('input', function () {
+  var valueLength = userNameInput.value.length;
+
+  if (valueLength < MIN_NAME_LENGTH) {
+    userNameInput.setCustomValidity('Ещё ' + (MIN_NAME_LENGTH - valueLength) +' симв.');
+  } else if (valueLength > MAX_NAME_LENGTH) {
+    userNameInput.setCustomValidity('Удалите лишние ' + (valueLength - MAX_NAME_LENGTH) +' симв.');
+  } else {
+    userNameInput.setCustomValidity('');
+  }
+});
+
+// Изминение цвета
+// currentColor Input в котором хранится текущее значение цвета
+// arrColors Массив цветов из которого нужно взять следующий цвет
+var getColor = function () {
+  var changes = [];
+  var change = {};
+  for (var i = 0; i < ; i++) {
+    change = {
+      wizardCoat: wizardCoat[getRandomInt(wizardsNames.length)]
+
+    }
+  }
+}
+
+wizardCoat.addEventListener('click', function () {
+  getColor(wizardCoat, coatColor, wizardCoatColor, 'fill', 'coat');
+});
+
+wizardEyes.addEventListener('click', function () {
+  getColor(wizardEyes, eyesColor, wizardEyesColor, 'fill', 'eyes');
+});
+
+wizardFireball.addEventListener('click', function () {
+  getColor(wizardFireball, fireballColor, wizardFireballColor, 'background-color', 'fireball');
+});
+
+
+var wizards = [];
+  var wizard = {};
+  for (var i = 0; i < WIZARD_NUMBERS; i++) {
+    wizard = {
+      name: wizardsNames[getRandomInt(wizardsNames.length)] + ' ' + wizardSurnames[getRandomInt(wizardSurnames.length)],
+      coatColor: coatColors[getRandomInt(coatColors.length)],
+      eyesColor: eyesColors[getRandomInt(eyesColors.length)]
+    };
+    wizards.push(wizard);
+  }
+  return wizards;
